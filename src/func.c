b1fec74300ed2fdf8dfdab6e5e993029c6056649
unknown <daniluk-tima@mail.ru>
2020-09-30 00:34:22 +0700

    Added WordsArray function at func.c

--- src/func.c
+++ src/func.c
@@ -32,3 +32,32 @@ void WordCount(long * max, long * count, char* filename)
     *count = counts;
     fclose(op);
 }
+
+void WordsArray(
+        char** slovaDyn, char* filename, long count, long max)
+{
+    char slova[count][max], symbol, pred;
+    int i = -1, j = 0;
+    FILE* op = fopen(filename, "r");
+    rewind(op);
+    pred = '.';
+    while ((symbol = getc(op)) != EOF) {
+        if ((symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123)) {
+            if (!((pred > 64 && pred < 91) || (pred > 96 && pred < 123))) {
+                i++;
+                j = 1;
+                if (symbol > 64 && symbol < 91)
+                    symbol += 32;
+                slovaDyn[i][0] = symbol;
+            } else {
+                if (symbol > 64 && symbol < 91)
+                    symbol += 32;
+                slovaDyn[i][j] = symbol;
+                j++;
+                slovaDyn[i][j] = 0;
+            }
+        }
+        pred = symbol;
+    }
+    fclose(op);
+}
