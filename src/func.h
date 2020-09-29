a0d72b4554418aeb644de71df88db9ef3c0954d2
unknown <daniluk-tima@mail.ru>
2020-09-30 00:49:15 +0700

    Added header Unique function at func.h

--- src/func.h
+++ src/func.h
@@ -4,8 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+int Unique(char** S, int s);
+
 void WordCount(long* max, long* count, char* filename);
 
 void WordsArray(char** slovaDyn, char* filename, long count, long max);
 
 void Sort(char** A, int size, int M);
+
+#endif
