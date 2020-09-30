65fc4a36bad76f29795cc38dbe4606fe78e1d138
unknown <daniluk-tima@mail.ru>
2020-09-30 16:18:54 +0700

    Edited func.c

--- src/func.c
+++ src/func.c
@@ -6,28 +6,31 @@
 #include <string.h>
 #include <time.h>
 
-void WordCount(long* max, long* count, char* filename)
-{
+void WordCount(long* max, long* count, char* filename) {
 	char symbol, pred = '.';
 	long len = 1;
 	long counts = 0;
 	FILE* op = fopen(filename, "r");
 	symbol = getc(op);
 	while ((symbol = getc(op)) != EOF) {
-		if ((symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123)
-			|| (symbol > 191 && symbol < 256)) {
-			if ((pred > 64 && pred < 91) || (pred > 96 && pred < 123)
-				|| (pred > 191 && pred < 256)) {
+		if ((symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123) ||
+			(symbol > 191 && symbol < 256)) {
+			if ((pred > 64 && pred < 91) || (pred > 96 && pred < 123) ||
+				(pred > 191 && pred < 256)) {
 				len++;
+
 			}
 			else {
 				if (len > * max)
 					*max = len;
 				len = 1;
 				counts++;
+
 			}
+
 		}
 		pred = symbol;
+
 	}
 	*max++;
 	*count = counts;
